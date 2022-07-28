5ede8504ebe80411d149a6e1e29e5ed6752f97a0
Eduardy <eduardylopes@gmail.com>
2022-07-28 13:37:25 -0300

    create subcription button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,31 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
+import Image from 'next/image';
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ignews</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>
-        Hello <span>World!</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <div className={styles.image}>
+          <Image src="/images/avatar.svg" alt="Girl coding" layout="fill" />
+        </div>
+      </main>
     </>
   );
 }

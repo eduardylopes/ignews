ceedfbb9d3ff67c96d086afa28b96380a4a56bbd
Eduardy <eduardylopes@gmail.com>
2022-07-28 10:28:01 -0300

    create global.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Head>
+        <title>ignews</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Hello <span>World!</span>
+      </h1>
+    </>
+  );
 }

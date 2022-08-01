06dfeeb7c84231d3355cb8ef21ac3016fc7446c0
Eduardy <eduardylopes@gmail.com>
2022-08-01 10:48:20 -0300

    implement stripe payment method

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import { fauna } from '../../../services/fauna';
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -18,9 +19,18 @@ export default NextAuth({
     async signIn({ user }) {
       const { email } = user;
 
-      await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
-
-      return true;
+      try {
+        await fauna.query(
+          q.If(
+            q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+            q.Create(q.Collection('users'), { data: { email } })
+          )
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
     },
   },
 });
